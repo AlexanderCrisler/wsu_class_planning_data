0a2aac37d66123e545d2866e45c276664a5cf18d
Alexander Crisler <alexcrisler@gmail.com>
2020-08-17 23:31:13 -0500

    created a .csv file with all of the class names for inputing into corona site

---
+++ class_html_parse.py
@@ -0,0 +1,21 @@
+import bs4
+import csv
+
+exampleFile = open('ClassList.html', encoding="utf8")
+exampleSoup = bs4.BeautifulSoup(exampleFile, 'html.parser')
+
+class_names = []
+for val in exampleSoup.select('h3'):
+    val_string = val.string
+    val_string = val_string.replace("\n","")
+    val_string = val_string.replace("\t","")
+    class_names.append(val_string.replace(" ",""))
+
+class_names_file = open('class_names_file.csv', 'w', newline='')
+outputWriter = csv.writer(class_names_file)
+separator = "-"
+for i in range(0, len(class_names)):
+    class_names[i] = class_names[i].split(separator, 1)[0]
+    
+outputWriter.writerow(class_names)
+print("finished.")
