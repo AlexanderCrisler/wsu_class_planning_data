6642233c3c53e7a8aae5cda5253e02cb30071a19
Alexander Crisler <alexcrisler@gmail.com>
2020-08-18 08:19:47 -0500

    added json file to replace csv

--- class_html_parse.py
+++ class_html_parse.py
@@ -1,5 +1,6 @@
 import bs4
 import csv
+import json
 
 exampleFile = open('ClassList.html', encoding="utf8")
 exampleSoup = bs4.BeautifulSoup(exampleFile, 'html.parser')
@@ -11,11 +12,12 @@ for val in exampleSoup.select('h3'):
     val_string = val_string.replace("\t","")
     class_names.append(val_string.replace(" ",""))
 
-class_names_file = open('class_names_file.csv', 'w', newline='')
-outputWriter = csv.writer(class_names_file)
+class_names_file = open('class_names_file.json', 'w', newline='')
+#outputWriter = csv.writer(class_names_file)
 separator = "-"
 for i in range(0, len(class_names)):
     class_names[i] = class_names[i].split(separator, 1)[0]
     
-outputWriter.writerow(class_names)
+#outputWriter.writerow(class_names)
+json.dump(class_names, class_names_file)
 print("finished.")
